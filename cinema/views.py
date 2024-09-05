877081e6b7148ed604e68509c82366927f07f75c
mykyta <nikita.fedotov222@gmail.com>
2024-09-05 13:24:31 +0300

    rewrite views

--- cinema/views.py
+++ cinema/views.py
@@ -12,54 +12,58 @@ from cinema.serializers import (
 
 
 class GenreList(APIView):
-    def get(self, request) -> Response:
+    def get(self, request):
         genres = Genre.objects.all()
         serializer = GenreSerializer(genres, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data)
 
-    def post(self, request) -> Response:
+    def post(self, request):
         serializer = GenreSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class GenreDetail(APIView):
+class GenreRetrieveUpdateDestroyAPIView(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Genre, pk=pk)
 
-    def get_object(self, pk: int) -> Genre:
-        return get_object_or_404(Genre, id=pk)
+    def get(self, request, pk):
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre)
+        return Response(serializer.data)
 
-    def get(self, request, pk: int) -> Response:
-        serializer = GenreSerializer(self.get_object(pk=pk))
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def put(self, request, pk):
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request, pk: int) -> Response:
-        serializer = GenreSerializer(self.get_object(pk=pk), data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    def patch(self, request, pk):
+        genre = self.get_object(pk)
+        serializer = GenreSerializer(genre, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def patch(self, request, pk: int) -> Response:
-        serializer = GenreSerializer(
-            self.get_object(pk=pk),
-            data=request.data,
-            partial=True
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def delete(self, request, pk: int) -> Response:
-        self.get_object(pk=pk).delete()
+    def delete(self, request, pk):
+        genre = self.get_object(pk)
+        genre.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class ActorList(generics.ListCreateAPIView):
+class ActorListCreateAPIView(generics.ListCreateAPIView):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
 
-class ActorDetail(generics.RetrieveUpdateDestroyAPIView):
+class ActorRetrieveUpdateDestroyAPIView(
+    generics.RetrieveUpdateDestroyAPIView
+):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
